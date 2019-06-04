b220040367260643fd55389faf66a9ea4b953f0d
Nick Morrow <nmorrow11@gmail.com>
2019-06-04 11:51:13 -0700

    set up server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+//configure db
+
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(morgan('combined'));
+
+app.listen(8081, () => {
+
+	console.log('listening on port 8081');
+});
